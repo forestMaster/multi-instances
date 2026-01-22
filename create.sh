798c5e6484bac31df7f11b275cd4c747f53df3f5
Eric Quinton <eric.quinton@proton.me>
2026-01-22 14:14:23 +0100

    :feat README: add license and README file

--- create.sh
+++ create.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
-# Création d'une nouvelle instance en s'appuyant sur la configuration présente dans le dossier migrate25
+# Create a new instance
 RACINE="/var/www/collec2App/"
 RACINESED="\/var\/www\/collec2App\/"
 CURRENT="${PWD}"
-VERSION="25.0"
+REPOSITORY="/var/local/collec-science/multi-instances"
+INSTANCES="$REPOSITORY/instances.csv"
+
+# Variables of environment
+DATABASE=collec_$INSTANCE
+LOGIN=collec
+PASSWORD=collecPassword
+URL=$INSTANCE.collec-science.inrae.fr
 
 # gestion des parametres
 OK=0
 while [ $OK == 0 ]
 do
-	read -p "Nom de l'instance à créer : " INSTANCE
-	read -p "Mode d'identification (CAS, CAS-BDD, BDD, HEADER) : " IDENTMODE
-	read -p "Premier login d'administration : " LOGINADMIN
-	echo "Nom de l'instance : "$INSTANCE
-	echo "Mode d'identification : "$IDENTMODE
-	echo "Login d'administration : "$LOGINADMIN
-	read -p "Créer l'instance [o/n/q] ? " REPONSE
-	if  [ $REPONSE == 'o' ] 
-	then
-		break;
-	elif [ $REPONSE == 'q' ] 
-	then
-		exit 0;
-	fi
+    read -p "Name of the instance to be created: " INSTANCE
+    read -p "Identification mode (CAS, CAS-BDD, BDD, HEADER) : " IDENTMODE
+    read -p "First admin login: " LOGINADMIN
+    echo "Name of the instance: "$INSTANCE
+    echo "Identification mode: "$IDENTMODE
+    echo "Administrator login: "$LOGINADMIN
+    read -p "Create the instance [y/n/q] ? " REPONSE
+    if  [ $REPONSE == 'y' ]
+    then
+        break;
+    elif [ $REPONSE == 'q' ]
+    then
+        exit 0;
+    fi
 done
 
-echo "Création de l'instance $INSTANCE"
+echo "Create the instance $INSTANCE"
 ENV="$RACINE/env"
 FOLDER="$RACINE$INSTANCE"
 FOLDERSED="$RACINESED$INSTANCE"
@@ -35,37 +42,33 @@ cd "$FOLDER"
 mkdir temp
 chmod g+w temp
 
-# Création des clés de chiffrement
+# Create the keys of encryption
 openssl genpkey -algorithm rsa -out id_collec -pkeyopt rsa_keygen_bits:2048
 openssl rsa -in id_collec -pubout -out id_collec.pub
 chmod 640 id_collec
 
-# Mise à niveau des parametres d'environnement
+# Update parameters of environment
 cp $ENV .env
-DATABASE=collec_$INSTANCE
-LOGIN=collec
-PASSWORD=XXXXXXX
-URL=$INSTANCE.collec-science.inrae.fr
+
 sed -i "s/https:\/\/collec.mysociety.com/https:\/\/$URL/" .env
 sed -i "s/database.default.database = collec/database.default.database = $DATABASE/" .env
-#sed -i "s/database.default.username = collec/database.default.username = $LOGIN/" .env
-sed -i "s/IdentificationConfig.identificationMode = CAS/IdentificationConfig.identificationMode = $IDENTMODE/" .env
+sed -i "s/IdentificationConfig.identificationMode = BDD/IdentificationConfig.identificationMode = $IDENTMODE/" .env
 sed -i "s/disableTotpToAdmin=1/disableTotpToAdmin=0/" .env
 sed -i "s/TEMP = /\#TEMP = /" .env
 sed -i "s/\#TEMP for multinstance/TEMP = \"$FOLDERSED\/temp\/\"/" .env
 sed -i "s/id_collec/$FOLDERSED\/id_collec/" .env
 sed -i "s/database.default.password = collecPassword/database.default.password = $PASSWORD/" .env
 
-# Mise à niveau des droits dans les dossiers
+# Update rights in directories
 chgrp -R www-data $FOLDER
 chmod 770 $FOLDER/temp
 
-# Traitement du virtual host apache
+# Generate Apache virtual host
 if [ $MODE == "HEADER" ] ;
 then
-	VHOST="/var/local/collec-science/migrate25/collec2-header.conf"
+    VHOST="$REPOSITORY/libs/collec2-header.conf"
 else
-	VHOST="/var/local/collec-science/migrate25/collec2.conf"
+    VHOST="$REPOSITORY/libs/collec2.conf"
 fi
 VHOSTNAME="/etc/apache2/sites-available/collec2-$INSTANCE.conf"
 cp $VHOST $VHOSTNAME
@@ -75,46 +78,56 @@ sed -i "s/collec-access.log/$INSTANCE-access.log/" $VHOSTNAME
 sed -i "s/collec-error.log/$INSTANCE-error.log/" $VHOSTNAME
 a2ensite collec2-$INSTANCE.conf
 
-echo "Création de la base de données"
-DBSCRIPT="/var/local/collec-science/migrate25/createDatabase.sql"
+# add directory site to manage rights (only one time)
+if [ ! -e /etc/apache2/sites-enabled/collecdirectory.conf ]; then
+    cp $REPOSITORY/collecdirectory.conf /etc/apache2/sites-available/
+    a2ensite collecdirectory.conf
+fi
+
+echo "Create the database"
+DBSCRIPT="$REPOSITORY/libs/createDatabase.sql"
 sed -i "s/dbcollec/$DATABASE/" $DBSCRIPT
 su postgres -c "psql -f $DBSCRIPT"
 sed -i "s/$DATABASE/dbcollec/" $DBSCRIPT
 
-# Ajout du premier compte d'administration
+# add the first account with admin rights
 
-SQL="insert into gacl.acllogin (login,logindetail) values ('$LOGINADMIN','administrateur')"
+SQL="insert into gacl.acllogin (login,logindetail) values ('$LOGINADMIN','administrator')"
 ADDRESS=postgresql://$LOGIN:$PASSWORD@localhost/$DATABASE
 psql $ADDRESS -c "$SQL"
 SQL="insert into gacl.acllogingroup (acllogin_id, aclgroup_id) values (2,1)"
 psql $ADDRESS -c "$SQL"
 
-if [ $IDENTMODE == "HEADER" ] 
+if [ $IDENTMODE == "HEADER" ]
 then
-	SQL="insert into gacl.logingestion (login, actif) values ('$LOGINADMIN', 1)"
-	psql $ADDRESS -c "$SQL"
+    SQL="insert into gacl.logingestion (login, actif) values ('$LOGINADMIN', 1)"
+    psql $ADDRESS -c "$SQL"
 fi
 
-# Mise à niveau des parametres dans dbparam
+# update parameters in dbparam
 SQL="update col.dbparam set dbparam_value = '$INSTANCE' where dbparam_name = 'APPLI_code';"
 psql $ADDRESS -c "$SQL"
 SQL="update col.dbparam set dbparam_value = 'Collec-Science - $INSTANCE' where dbparam_name = 'otp_issuer' or dbparam_name = 'APPLI_title';"
 psql $ADDRESS -c "$SQL"
 
-# Ajout de l'instance dans le fichier instances.csv
-INSTANCES="/var/local/collec-science/instances.csv"
-echo "$INSTANCE;$VERSION;localhost;collec_$INSTANCE;collec;adm1n0verall;$VERSION;$IDENTMODE;$URL;" >> $INSTANCES
+# Add the instance in the instances.csv file
+if [ ! -e $INSTANCES ]; then
+    cp $DIRECTORY/instances-dist.csv $DIRECTORY/instances.csv
+fi
+
+echo "$INSTANCE;localhost;$DATABASE;$LOGIN;$PASSWORD;" >> $INSTANCES
 
 echo "============="
-echo "Fin de création de l'instance"
-echo "Si vous avez activé une identification CAS ou HEADER, vous devrez réaliser les demandes d'accès correspondantes"
-echo "Vérifiez si vous avez eu des messages d'erreur dans les scripts qui ont été exécutés"
-echo "Vérifiez également le fichier $VHOSTNAME, pour le paramétrage Apache"
-echo "et le fichier $FOLDER/.env, qui contient les paramètres spécifiques de l'instance"
+echo "End of creation of the instance"
+echo "If you have activate an identification CAS or HEADER, you will need to make the relevant access requests"
+echo "Verify if you had error messages into the executed scripts"
+echo "Also check the $VHOSTNAME file for the Apache settings"
+echo "and the $FOLDER/.env file, which contains the specific settings of the instance"
 echo ""
-echo "Une fois que tout est ok, vous pouvez activer l'instance avec la commande :"
+echo "When all is ok, you can activate the instance with this command:"
 echo "systemctl reload apache2"
-echo "Enfin, vérifiez le contenu du fichier $INSTANCES, puis enregistrez-le dans le dépôt (git add *, git status, git commit -m 'création de l'instance $INSTANCE', git push" 
+echo "Then, verify the content of $INSTANCES file."
+echo "That's all, folks !"
 echo "============="
 cd $CURRENT
 
