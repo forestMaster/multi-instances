5abf6d683b9a539b0272886a0d419de7c24d05a0
forestMaster <christopher.bilodeau@nrcan-rncan.gc.ca>
2026-05-07 15:32:50 -0400

    Update createDatabase.sql
    added var __zzz__ to make sed better. added on error stop, added atomic collec_create and added create first user collec if not there in fresh instances

--- libs/createDatabase.sql
+++ libs/createDatabase.sql
@@ -1,8 +1,27 @@
+\set ON_ERROR_STOP on
+
+/*
+ * User creation
+ */
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '__DBUSER__') THEN
+    CREATE USER __DBUSER__ WITH
+      LOGIN
+      NOSUPERUSER
+      INHERIT
+      NOCREATEDB
+      NOCREATEROLE
+      NOREPLICATION
+      PASSWORD '__DBPASS__';
+  END IF;
+END $$;
+
 /*
  * Database creation
  */
-create database dbcollec owner collec;
-\c "dbname=dbcollec"
+create database __DBNAME__ owner __DBUSER__;
+\c "dbname=__DBNAME__"
  create extension if not exists postgis schema public;
  create extension if not exists pgcrypto schema public;
  create extension if not exists pg_trgm schema pg_catalog;
@@ -13,4 +32,7 @@ create database dbcollec owner collec;
 /**
  * create structure
  */
+BEGIN;
+\c "dbname=__DBNAME__ user=__DBUSER__ password=__DBPASS__ host=localhost"
 \ir /var/www/collec2App/collec-science/install/pgsql/collec_create.sql
+COMMIT;
